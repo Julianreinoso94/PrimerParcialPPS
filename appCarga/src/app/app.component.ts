0e935058bb258b88c7b5f04ac3d444d8a836bfa2
julianreinoso94 <julianreinoso94@gmail.com>
2017-10-01 17:31:25 -0300

    Registro y Login

--- appCarga/src/app/app.component.ts
+++ appCarga/src/app/app.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform , Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+ 
+  @ViewChild(Nav) nav: Nav;
+  rootPage:any;
+  pages: Array<{title: string, component: any}>
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+//rootPage:any = HomePage;
+
+
+
+  constructor(platform: Platform, 
+    private AfA: AngularFireAuth,statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+        const authObserver = this.AfA.authState.subscribe( user => {
+        if (user) {
+          this.rootPage = HomePage;
+          authObserver.unsubscribe();
+        } else {
+          this.rootPage = LoginPage;
+          authObserver.unsubscribe();
+        }
+      });    
+
+   this.pages = [
+    
+      { title: 'Salir', component: LoginPage },
+    ];
   }
+
 }
 
--- appCarga/src/app/app.module.ts
+++ appCarga/src/app/app.module.ts
@@ -6,21 +6,50 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+
+//PLUGGIN
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+
+//FIREBASE
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { HttpModule } from '@angular/http';
+
+//DB
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+var firebaseAuth  = {
+  apiKey: "AIzaSyCDLDZtnlim-MHyW30KYNN1jLiMjB6t-tI",
+  authDomain: "appcarga-d9895.firebaseapp.com",
+  databaseURL: "https://appcarga-d9895.firebaseio.com",
+  projectId: "appcarga-d9895",
+  storageBucket: "appcarga-d9895.appspot.com",
+  messagingSenderId: "1930562520"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAuth),
+    AngularFireAuthModule, HttpModule,
+     AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
